81ec85ec979c6d46d9b7d7c586826791c3a9a59f
abdulrahmansabah <152287517+abdulrahmansabah@users.noreply.github.com>
2026-05-10 15:50:09 +0200

    AS : Added a unit testing template

---
+++ frontend/src/pages/Example/Example.jsx
@@ -0,0 +1,10 @@
+
+
+//Just a small html page - for the testing template
+export default function TestOne() {
+  return (
+    <div>
+      <h1>Example Page</h1>
+    </div>
+  );
+}
---
+++ frontend/src/pages/Example/Example.test.jsx
@@ -0,0 +1,18 @@
+import { render, screen } from "@testing-library/react";
+import { describe, it, expect } from "vitest";
+import Example from "./Example";
+
+
+//The Name of the page
+describe("Example", () => {
+
+  //In here, what you are testing
+  it("Test One", () => {
+    //in here, we put that specific import
+    render(<Example />);
+
+    //in here you can check buttons etc...
+    expect(screen.getByText("Example Page")).toBeDefined();
+  });
+
+});
