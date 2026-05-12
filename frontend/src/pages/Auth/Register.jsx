2d5e32e6fb3f80fcef5635244dead49dd88d3355
antonyvs05 <vanstratenantony@gmail.com>
2026-05-12 21:22:48 +0200

    AVS: feat: finished adding password validation and appropriate names for css styling.

--- frontend/src/pages/Auth/Register.jsx
+++ frontend/src/pages/Auth/Register.jsx
@@ -15,8 +15,8 @@ export const Register = () => {
 
 
 const form = useForm();
-const {register, control, handleSubmit,watch} = form;
-
+const {register, control, handleSubmit,watch, formState} = form;
+const{errors} = formState;
 const onSubmit = (initialValues) =>{
     console.log('Form Submitted', initialValues)
 }
@@ -27,10 +27,12 @@ const password = watch("password");
     //Mock Page
     <div>
          <form onSubmit = {handleSubmit(onSubmit)} noValidate>
+            <div className = "form-control">
             <label htmlFor = "full_name">Full Name</label>
             <input type = "text" id = "full_name" {...register("full_name",
                 {required: "Full name is required",}
             )}/>
+            <p className = "error">{errors.full_name?.message}</p>
 
             <label htmlFor = "email">E-mail</label>
             <input type = "email" id = "email"  {...register("email",
@@ -42,24 +44,29 @@ const password = watch("password");
                     value: /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/,
                     message: "Invalid Email"
                 }})}/>
-
+                <p className = "error">{errors.email?.message}</p>
+                </div>
+               <div className = "form-control">
             <label htmlFor = "password">Password</label>
             <input type = "text" id = "password" {...register("password",
                 {required: {
                     value: true,
                     message: "Password is required"},
                     pattern:{
-                        value: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&]{8,}$/,
-                        message: "Password has to have one uppercase, one number, one character and atleast 8 characters long"
+                        value: /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[@$!%*?&])[A-Za-z\d@$!%*?&#.]{8,}$/,
+                        message: "Password has to have one uppercase, one number, one character and atleast 8 characters long or illegal character detected"
                 }})}/>
-
+                <p className = "error">{errors.password?.message}</p>
+                </div> 
+                <div className = "form-control">
             <label htmlFor = "confirm_password">Confirm Password</label>
             <input type = "text" id = "confirm_password" {...register("confirm_password",
                 {required: {
                     value: true,
                     message: "Confirmed password is required"},
                     validate: (value) => value === password || "Passwords do not match"})}/>
-
+                    <p className = "error">{errors.confirm_password?.message}</p>
+                    </div>
             <button>Submit</button>
         </form>
         <DevTool control = {control}/>
