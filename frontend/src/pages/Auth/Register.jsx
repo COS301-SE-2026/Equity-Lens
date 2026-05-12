1bc2ee1e3a22374810b063bda68ef39598d125cf
antonyvs05 <vanstratenantony@gmail.com>
2026-05-12 18:31:20 +0200

    AVS: feat: Succesfully created values to be returned and now form returns object that can be interpreted.

--- frontend/src/pages/Auth/Register.jsx
+++ frontend/src/pages/Auth/Register.jsx
@@ -2,29 +2,37 @@ import React from 'react'
 import {useForm} from 'react-hook-form'
 import {DevTool} from '@hookform/devtools'
 
+const initialValues = {
+    full_name : '',
+    email : '',
+    password : '',
+    confirm_password : ''
+}
+
+
 export const Register = () => {
 
 const form = useForm();
 const {register, control, handleSubmit} = form;
 
-const onSubmit = (data) =>{
-    console.log('Form Submitted', data)
+const onSubmit = (initialValues) =>{
+    console.log('Form Submitted', initialValues)
 }
 
   return (
     //Mock Page
     <div>
          <form onSubmit = {handleSubmit(onSubmit)}>
-            <label htmlfor = "full_name">Full Name</label>
+            <label htmlFor = "full_name">Full Name</label>
             <input type = "text" id = "full_name" {...register("full_name")}/>
 
-            <label htmlfor = "email">E-mail</label>
+            <label htmlFor = "email">E-mail</label>
             <input type = "email" id = "email"  {...register("email")}/>
 
-            <label htmlfor = "password">Password</label>
+            <label htmlFor = "password">Password</label>
             <input type = "text" id = "password" {...register("password")}/>
 
-            <label htmlfor = "confirm_password">Confirm Password</label>
+            <label htmlFor = "confirm_password">Confirm Password</label>
             <input type = "text" id = "confirm_password" {...register("confirm_password")}/>
 
             <button>Submit</button>
