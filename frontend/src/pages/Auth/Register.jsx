0c09a1c83026e4da3076b2fed2698a78f4dd9a6d
antonyvs05 <vanstratenantony@gmail.com>
2026-05-12 18:23:44 +0200

    AVS: feat: added hookform devtools using npm install -D @hookform/devtools and created supporting functions for submission.

--- frontend/src/pages/Auth/Register.jsx
+++ frontend/src/pages/Auth/Register.jsx
@@ -1,15 +1,20 @@
 import React from 'react'
 import {useForm} from 'react-hook-form'
+import {DevTool} from '@hookform/devtools'
 
 export const Register = () => {
 
 const form = useForm();
-const {register} = form;
+const {register, control, handleSubmit} = form;
+
+const onSubmit = (data) =>{
+    console.log('Form Submitted', data)
+}
 
   return (
     //Mock Page
     <div>
-         <form>
+         <form onSubmit = {handleSubmit(onSubmit)}>
             <label htmlfor = "full_name">Full Name</label>
             <input type = "text" id = "full_name" {...register("full_name")}/>
 
@@ -24,6 +29,7 @@ const {register} = form;
 
             <button>Submit</button>
         </form>
+        <DevTool control = {control}/>
     </div>
    
   )
