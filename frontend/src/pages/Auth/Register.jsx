41599cfd6c0c98a87ed6d25071bc4870d9371608
antonyvs05 <vanstratenantony@gmail.com>
2026-05-12 18:13:05 +0200

    AVS: feat: added register method using react hook form and using it for inputs.

--- frontend/src/pages/Auth/Register.jsx
+++ frontend/src/pages/Auth/Register.jsx
@@ -1,21 +1,26 @@
 import React from 'react'
+import {useForm} from 'react-hook-form'
 
 export const Register = () => {
+
+const form = useForm();
+const {register} = form;
+
   return (
     //Mock Page
     <div>
          <form>
             <label htmlfor = "full_name">Full Name</label>
-            <input type = "text" id = "full_name" name = "full_name"/>
+            <input type = "text" id = "full_name" {...register("full_name")}/>
 
             <label htmlfor = "email">E-mail</label>
-            <input type = "email" id = "email" name = "email"/>
+            <input type = "email" id = "email"  {...register("email")}/>
 
             <label htmlfor = "password">Password</label>
-            <input type = "text" id = "password" name = "password"/>
+            <input type = "text" id = "password" {...register("password")}/>
 
             <label htmlfor = "confirm_password">Confirm Password</label>
-            <input type = "text" id = "confirm_password" name = "confirm_password"/>
+            <input type = "text" id = "confirm_password" {...register("confirm_password")}/>
 
             <button>Submit</button>
         </form>
