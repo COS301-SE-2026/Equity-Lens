58739e500e8a9b8cb69a9a77bf660739b2de972e
abdulrahmansabah <152287517+abdulrahmansabah@users.noreply.github.com>
2026-05-14 20:54:41 +0200

    AS: Added the footer and navigation bar

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,15 +1,18 @@
 import ThemeToggle from './components/common/ThemeToggle/ThemeToggle.jsx'
 import Login from './pages/Auth/Login'
+import Footer from './components/Footer'
+import Navbar from './components/Navbar'
 
 
 function App() {
   return (
     <div style={{ background: 'var(--bg)', color: 'var(--text-primary)', minHeight: '100vh' }}>
-      <h1>EquityLens</h1>
-      <p>Frontend running</p>
+       <Navbar />
       {/* This is the Theme toggling button */}
       <ThemeToggle />
         <Login />
+
+          <Footer />
     </div>
   )
 }
