d50bb0e1d0f0de1ad898da082ffff8c0f269b2eb
abdulrahmansabah <152287517+abdulrahmansabah@users.noreply.github.com>
2026-05-17 15:16:15 +0200

    AS : Fixed and updated the eslintrc.

---
+++ frontend/.eslintrc.cjs
@@ -0,0 +1,10 @@
+module.exports = {
+  root: true,
+
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+  ],
+
+  plugins: ['react'],//this plugin in order to allow the react on top to work
+};
