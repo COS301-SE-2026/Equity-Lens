51e4815e2dbbdc393f8528637c7983ad2fb8289b
antonyvs05 <vanstratenantony@gmail.com>
2026-05-18 20:24:25 +0200

    AVS: feat: Implemented RSI function and debugged altman_z_score function.

--- backend/app/indicators/altman_z_score.py
+++ backend/app/indicators/altman_z_score.py
@@ -3,7 +3,7 @@ def caclculate_altman_zscore(working_capital, total_assets, retained_earnings, e
     Z1 = working_capital / total_assets
     Z2 = retained_earnings / total_assets
     Z3 = ebit / total_assets
-    Z4 = market_cap / total_assets
+    Z4 = market_cap / total_liabilities
     Z5 = sales / total_assets
 
     z_score = (1.2 * Z1) + (1.4 * Z2) + (3.3 * Z3) + (0.6 * Z4) + (0.999 * Z5)
---
+++ backend/app/indicators/rsi.py
@@ -0,0 +1,18 @@
+import pandas as pd
+"Relative Strength Index (RSI)"
+"Scores price movement from 0-100 to warn when something is overbought or oversold."
+def calulcate_rsi(price_series, periods = 14):
+    prices = pd.Series(price_series)
+
+    delta = prices.diff()
+
+    gain = delta.clip(lower = 0)
+    loss = -delta.clip(upper = 0)
+    #Wilder's Exponential Moving average
+    avg_gain = gain.ewm(com=periods - 1,adjust=False).mean()
+    avg_loss = loss.ewm(com=periods - 1,adjust=False).mean()
+
+    rs = avg_gain / avg_loss
+    #Convert to scale form
+    rsi = 100 - (100 / (1 + rs))
+    return rsi
