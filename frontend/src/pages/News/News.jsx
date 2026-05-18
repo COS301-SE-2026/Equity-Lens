668f1570dabf04ae2e64bb9db14df7017e2cf514
abdulrahmansabah <152287517+abdulrahmansabah@users.noreply.github.com>
2026-05-18 23:29:08 +0200

    AS : Added the feedback form the pull request.

--- frontend/src/pages/News/News.jsx
+++ frontend/src/pages/News/News.jsx
@@ -1,10 +1,11 @@
 import { useEffect, useState } from "react";
+import { API_BASE_URL_NEWS } from "../../utils/constants";
 
 const NewsInvestment = () => {
   const [articles, setArticles] = useState([]);
 
   useEffect(() => {
-    fetch("http://localhost:8000/api/news")
+    fetch(`${API_BASE_URL_NEWS}/api/news`)
       .then((thedata) => thedata.json())
       .then((settingdata) => setArticles(settingdata.articles || []))
       .catch((error) => console.log(error));
@@ -18,9 +19,11 @@ const NewsInvestment = () => {
         <div key={index}>
           <h2>{article.title}</h2>
           <p>{article.description}</p>
+
           <a href={article.url}>
             click in here to go to,or to read more information
           </a>
+
           <hr />
         </div>
       ))}
