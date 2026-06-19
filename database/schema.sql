39e2830608bf005fcec256767fd50f3a6046ae49
abdulrahmansabah <152287517+abdulrahmansabah@users.noreply.github.com>
2026-06-19 14:49:55 +0200

    updated : Added more tables

--- database/schema.sql
+++ database/schema.sql
@@ -43,29 +43,98 @@ CREATE TABLE IF NOT EXISTS documents
 );
 
 
-CREATE TABLE IF NOT EXISTS news_articles 
+CREATE TABLE IF NOT EXISTS portfolios 
 (
 
     id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
-
     user_id UUID NOT NULL REFERENCES users(id) ON DELETE CASCADE,
+    document_id UUID NOT NULL REFERENCES documents(id) ON DELETE CASCADE,
 
-    document_id NOT NULL REFERENCES documents(id) ON DELETE CASCADE,
+    account_number VARCHAR(100),
+    portfolio_name VARCHAR(255),
 
-    stock_symbol VARCHAR(255) NOT NULL,
-
-    source_name VARCHAR(255),
-    author VARCHAR(255),
-
-    title TEXT NOT NULL,
-    description TEXT,
-
-    article_url TEXT NOT NULL UNIQUE,
-    image_url TEXT,
-
-    published_at TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
+    statement_start_date VARCHAR(255),
+    statement_end_date VARCHAR(255),
 
     created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
     updated_at TIMESTAMP WITH TIME ZONE DEFAULT NOW()
 );
+
+
+CREATE TABLE IF NOT EXISTS holdings 
+(
+
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    portfolio_id UUID NOT NULL REFERENCES portfolios(id) ON DELETE CASCADE,
+
+    instrument_name VARCHAR(100),
+    quantity NUMERIC(18,4),
+
+    total_cost NUMERIC(18,2),
+    cost_price NUMERIC(18,2),
+
+    current_price NUMERIC(18,2),
+    current_value NUMERIC(18,2),
+
+    weight_percentage NUMERIC(18,2),
+
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
+    updated_at TIMESTAMP WITH TIME ZONE DEFAULT NOW()
+);
+
+CREATE TABLE IF NOT EXISTS Instrument_Purchases_and_Sales 
+(
+
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    portfolio_id UUID NOT NULL REFERENCES portfolios(id) ON DELETE CASCADE,
+
+    transactions_date VARCHAR(100),
+    transactions_type NUMERIC(18,4),
+
+    transactions_name NUMERIC(18,2),
+    price NUMERIC(18,2),
+
+    quantity NUMERIC(18,2),
+    transactions_cost NUMERIC(18,2),
+
+    Value_Zar NUMERIC(18,2),
+
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
+    updated_at TIMESTAMP WITH TIME ZONE DEFAULT NOW()
+);
+
+CREATE TABLE IF NOT EXISTS  Transaction_Costs 
+(
+
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    portfolio_id UUID NOT NULL REFERENCES portfolios(id) ON DELETE CASCADE,
+
+    Instrument VARCHAR(100),
+    Brokerage NUMERIC(18,4),
+
+    Other_Trading_Costs NUMERIC(18,2),
+
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
+    updated_at TIMESTAMP WITH TIME ZONE DEFAULT NOW()
+);
+
+
+CREATE TABLE IF NOT EXISTS  Contributions_and_Withdrawals
+(
+
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+    portfolio_id UUID NOT NULL REFERENCES portfolios(id) ON DELETE CASCADE,
+
+    transaction_date VARCHAR(100),
+    Settlement_Date NUMERIC(18,4),
+
+    transaction_type NUMERIC(18,2),
+    value_ZAR NUMERIC(18,2),
+
+    created_at TIMESTAMP WITH TIME ZONE DEFAULT NOW(),
+    updated_at TIMESTAMP WITH TIME ZONE DEFAULT NOW()
+);
+
+
+
 
