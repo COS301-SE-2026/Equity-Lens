12bf2ded5bfd3861eabed98e4f2415e273a2631f
abdulrahmansabah <152287517+abdulrahmansabah@users.noreply.github.com>
2026-06-20 18:40:38 +0200

    Fix : updated and fixed to have all the cases the same, such as PascalCase and snake_case

--- backend/app/models/portfolio.py
+++ backend/app/models/portfolio.py
@@ -4,8 +4,8 @@ from sqlalchemy import Column, String, Boolean, DateTime, ForeignKey, Text, Date
 from sqlalchemy.dialects.postgresql import UUID
 from app.database import Base
 
-class transaction_type(Base):
-    __tablename__ = "transaction_type"
+class TransactionType(Base):
+    __tablename__ = "transaction_types"
 
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
 
@@ -15,8 +15,8 @@ class transaction_type(Base):
     created_at = Column(DateTime, default=lambda: datetime.now(timezone.utc))
     updated_at = Column(DateTime,default=lambda: datetime.now(timezone.utc),onupdate=lambda: datetime.now(timezone.utc))
 
-class instrument_type(Base):
-    __tablename__ = "instrument_type"
+class InstrumentType(Base):
+    __tablename__ = "instrument_types"
 
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
 
@@ -40,7 +40,7 @@ class Document(Base):
     created_at = Column(DateTime, default=lambda: datetime.now(timezone.utc))
     updated_at = Column(DateTime,default=lambda: datetime.now(timezone.utc),onupdate=lambda: datetime.now(timezone.utc))
 
-class portfolios(Base):
+class Portfolios(Base):
     __tablename__ = "portfolios"
 
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
@@ -55,117 +55,117 @@ class portfolios(Base):
     created_at = Column(DateTime, default=lambda: datetime.now(timezone.utc))
     updated_at = Column(DateTime,default=lambda: datetime.now(timezone.utc),onupdate=lambda: datetime.now(timezone.utc))
 
-class holdings(Base):
+class Holdings(Base):
     __tablename__ = "holdings"
 
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     portfolio_id = Column(UUID(as_uuid=True), ForeignKey("portfolios.id",ondelete="CASCADE"), nullable=False)
 
     instrument_name = Column(String(100), nullable=False)
-    quantity = Column(Numeric(18,4)),
-    total_cost = Column(Numeric(18,2)),
-    cost_price = Column(Numeric(18,2)),
-    current_price = Column(Numeric(18,2)),
-    current_value = Column(Numeric(18,2)),
-    weight_percentage = Column(Numeric(18,2)),
+    quantity = Column(Numeric(18,4))
+    total_cost = Column(Numeric(18,2))
+    cost_price = Column(Numeric(18,2))
+    current_price = Column(Numeric(18,2))
+    current_value = Column(Numeric(18,2))
+    weight_percentage = Column(Numeric(18,2))
 
     created_at = Column(DateTime, default=lambda: datetime.now(timezone.utc))
     updated_at = Column(DateTime,default=lambda: datetime.now(timezone.utc),onupdate=lambda: datetime.now(timezone.utc))
 
 
-class Instrument_Purchases_and_Sales(Base):
+class InstrumentPurchasesAndSales(Base):
     __tablename__ = "instrument_purchases_and_sales"
 
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     portfolio_id = Column(UUID(as_uuid=True), ForeignKey("portfolios.id",ondelete="CASCADE"), nullable=False)
 
-    transactions_date = Column(Date),
-    transaction_type_id = Column(UUID(as_uuid=True), ForeignKey("transaction_type.id",ondelete="CASCADE"), nullable=False)
-    instrument_type_id = Column(UUID(as_uuid=True), ForeignKey("instrument_type.id",ondelete="CASCADE"), nullable=False)
-    price = Column(Numeric(18,2)),
-    quantity = Column(Numeric(18,4)),
-    transactions_cost = Column(Numeric(18,2)),
-    Value_Zar = Column(Numeric(18,2)),
+    transactions_date = Column(Date)
+    transaction_type_id = Column(UUID(as_uuid=True), ForeignKey("transaction_types.id",ondelete="CASCADE"), nullable=False)
+    instrument_type_id = Column(UUID(as_uuid=True), ForeignKey("instrument_types.id",ondelete="CASCADE"), nullable=False)
+    price = Column(Numeric(18,2))
+    quantity = Column(Numeric(18,4))
+    transactions_cost = Column(Numeric(18,2))
+    value_zar = Column(Numeric(18,2))
 
     created_at = Column(DateTime, default=lambda: datetime.now(timezone.utc))
     updated_at = Column(DateTime,default=lambda: datetime.now(timezone.utc),onupdate=lambda: datetime.now(timezone.utc))
 
 
-class Transaction_Costs(Base):
+class TransactionCosts(Base):
     __tablename__ = "transaction_costs"
 
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     portfolio_id = Column(UUID(as_uuid=True), ForeignKey("portfolios.id",ondelete="CASCADE"), nullable=False)
 
-    transaction_type_id = Column(UUID(as_uuid=True), ForeignKey("transaction_type.id",ondelete="CASCADE"), nullable=False)
-    Brokerage = Column(Numeric(18,2)),
-    Other_Trading_Costs = Column(Numeric(18,2)),
+    transaction_type_id = Column(UUID(as_uuid=True), ForeignKey("transaction_types.id",ondelete="CASCADE"), nullable=False)
+    brokerage = Column(Numeric(18,2))
+    other_trading_costs = Column(Numeric(18,2))
 
     created_at = Column(DateTime, default=lambda: datetime.now(timezone.utc))
     updated_at = Column(DateTime,default=lambda: datetime.now(timezone.utc),onupdate=lambda: datetime.now(timezone.utc))
 
 
-class Contributions_and_Withdrawals(Base):
+class ContributionsAndWithdrawals(Base):
     __tablename__ = "contributions_and_withdrawals"
 
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     portfolio_id = Column(UUID(as_uuid=True), ForeignKey("portfolios.id",ondelete="CASCADE"), nullable=False)
 
   
-    transaction_date = Column(Date),
-    Settlement_Date = Column(Date),
-    transaction_type_id = Column(UUID(as_uuid=True), ForeignKey("transaction_type.id",ondelete="CASCADE"), nullable=False)
-    value_ZAR = Column(Numeric(18,2)),
+    transaction_date = Column(Date)
+    settlement_date = Column(Date)
+    transaction_type_id = Column(UUID(as_uuid=True), ForeignKey("transaction_types.id",ondelete="CASCADE"), nullable=False)
+    value_zar = Column(Numeric(18,2))
 
     created_at = Column(DateTime, default=lambda: datetime.now(timezone.utc))
     updated_at = Column(DateTime,default=lambda: datetime.now(timezone.utc),onupdate=lambda: datetime.now(timezone.utc))
 
-class Dividends_and_Withholding_Tax(Base):
+class DividendsAndWithholdingTax(Base):
     __tablename__ = "dividends_and_withholding_tax"
 
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     portfolio_id = Column(UUID(as_uuid=True), ForeignKey("portfolios.id",ondelete="CASCADE"), nullable=False)
 
   
-    transaction_date = Column(Date),
-    instrument_type_id = Column(UUID(as_uuid=True), ForeignKey("instrument_type.id",ondelete="CASCADE"), nullable=False)
-    Gross_dividend = Column(Numeric(18,2)),
-    Withholding_tax = Column(Numeric(18,2)),
-    Net_dividend = Column(Numeric(18,2)),
-    Tax_rate = Column(Numeric(18,2)),
+    transaction_date = Column(Date)
+    instrument_type_id = Column(UUID(as_uuid=True), ForeignKey("instrument_types.id",ondelete="CASCADE"), nullable=False)
+    gross_dividend = Column(Numeric(18,2))
+    withholding_tax = Column(Numeric(18,2))
+    net_dividend = Column(Numeric(18,2))
+    tax_rate = Column(Numeric(18,2))
 
     created_at = Column(DateTime, default=lambda: datetime.now(timezone.utc))
     updated_at = Column(DateTime,default=lambda: datetime.now(timezone.utc),onupdate=lambda: datetime.now(timezone.utc))
 
 
-class transaction_Interest(Base):
+class TransactionInterest(Base):
     __tablename__ = "transaction_interest"
 
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     portfolio_id = Column(UUID(as_uuid=True), ForeignKey("portfolios.id",ondelete="CASCADE"), nullable=False)
 
   
-    transaction_date = Column(Date),
-    Settlement_Date = Column(Date),
-    transaction_type_id = Column(UUID(as_uuid=True), ForeignKey("transaction_type.id",ondelete="CASCADE"), nullable=False)
-    instrument_type_id = Column(UUID(as_uuid=True), ForeignKey("instrument_type.id",ondelete="CASCADE"), nullable=False)    
-    value_ZAR = Column(Numeric(18,2)),
+    transaction_date = Column(Date)
+    settlement_date = Column(Date)
+    transaction_type_id = Column(UUID(as_uuid=True), ForeignKey("transaction_types.id",ondelete="CASCADE"), nullable=False)
+    instrument_type_id = Column(UUID(as_uuid=True), ForeignKey("instrument_types.id",ondelete="CASCADE"), nullable=False)    
+    value_zar = Column(Numeric(18,2))
 
     created_at = Column(DateTime, default=lambda: datetime.now(timezone.utc))
     updated_at = Column(DateTime,default=lambda: datetime.now(timezone.utc),onupdate=lambda: datetime.now(timezone.utc))
 
 
-class transaction_Expenses(Base):
+class TransactionExpenses(Base):
     __tablename__ = "transaction_expenses"
 
     id = Column(UUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
     portfolio_id = Column(UUID(as_uuid=True), ForeignKey("portfolios.id",ondelete="CASCADE"), nullable=False)
   
-    transaction_date = Column(Date),
-    Settlement_Date = Column(Date),
-    transaction_type_id = Column(UUID(as_uuid=True), ForeignKey("transaction_type.id",ondelete="CASCADE"), nullable=False)
-    narrative = Column(Text),
-    value_ZAR = Column(Numeric(18,2)),
+    transaction_date = Column(Date)
+    settlement_date = Column(Date)
+    transaction_type_id = Column(UUID(as_uuid=True), ForeignKey("transaction_types.id",ondelete="CASCADE"), nullable=False)
+    narrative = Column(Text)
+    value_zar = Column(Numeric(18,2))
 
     created_at = Column(DateTime, default=lambda: datetime.now(timezone.utc))
     updated_at = Column(DateTime,default=lambda: datetime.now(timezone.utc),onupdate=lambda: datetime.now(timezone.utc))
