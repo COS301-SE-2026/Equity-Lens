7e0b81771745b7b786c6957c92b4f17645f9f077
jonty282 <u23536862@tuks.co.za>
2026-06-25 18:38:03 +0200

    Added client response functionality

--- backend/app/services/ai_service.py
+++ backend/app/services/ai_service.py
@@ -10,3 +10,20 @@ def get_bedrock_client():
         secret_access_key = settings.aws_secret_access_key_id
     )
 
+#now for chat functionality 
+def chat(user_message: str):
+    client = get_bedrock_client()
+    #response (using converse uses modelId and JSON format for messages)
+    response = client.converse(
+        modelId = settings.bedrock_model,
+        #for user
+        messages = [
+            {
+                "role": "user",
+                "content": [{"text": user_message}]
+            }
+        ],
+        system = [{"text": "You are an AI financial asistant for EquityLens."}],
+        inferenceConfig = {"maxTokens": 256}
+    )
+    return response["output"]["message"]["content"][0]["text"]
