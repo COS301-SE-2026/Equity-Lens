574d6d08bd83244694fa45ddd331face060bb8bd
antonyvs05 <vanstratenantony@gmail.com>
2026-07-03 14:52:58 +0800

    debug: debugging the api hoisting

--- frontend/src/router/AppRouter.test.jsx
+++ frontend/src/router/AppRouter.test.jsx
@@ -1,7 +1,11 @@
 import { describe, it, expect, vi, beforeEach } from 'vitest';
-vi.mock("../utils/constants", () => ({
-  API_BASE_URL: "http://localhost:8000",
-}));
+vi.mock("../utils/constants", async (importOriginal)  => {
+  const actual = await importOriginal();
+  return{
+    ...actual,
+    API_BASE_URL: "http://localhost:8000",
+  };
+});
 import { render, screen, waitFor } from '@testing-library/react';
 import AppRouter from './AppRouter';
 
