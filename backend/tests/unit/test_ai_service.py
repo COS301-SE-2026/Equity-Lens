f820c73b52c8190c20037dd8a1ce9a75521e2b02
jonty282 <u23536862@tuks.co.za>
2026-07-07 21:44:54 +0200

    Added document data to unit test for test portfolio with data

--- backend/tests/unit/test_ai_service.py
+++ backend/tests/unit/test_ai_service.py
@@ -1,5 +1,5 @@
 from app.services.ai_service import get_user_portfolio_context
-from app.models.portfolio import Portfolios
+from app.models.portfolio import Portfolios, Document
 
 def test_portfolio_linked_no_data(db_session, test_user):
     ai_reply = get_user_portfolio_context(db_session, test_user.id)
@@ -11,11 +11,21 @@ def test_portfolio_with_data(db_session, test_user):
         account_number = "U23-536",
         portfolio_name = "The invesment portfolio of mine from Easy Equities"
     )
-
     db_session.add(portfolio)
+
+    document = Document(
+        user_id = test_user.id,
+        file_name = "portfolio.pdf",
+        encrypted_file_path = "example/path/portfolio.pdf",
+        encrypted_document_text = "Amount: R3 000"
+    )
+    db_session.add(document)
+
     db_session.commit()
 
     output = get_user_portfolio_context(db_session, test_user.id)
     assert "The invesment portfolio of mine from Easy Equities" in output
     assert "U23-536" in output
+    assert "portfolio.pdf" in output
+    assert "Amount: R3 000" in output
     
