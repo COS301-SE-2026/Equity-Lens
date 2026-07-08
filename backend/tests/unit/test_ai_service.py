bfb0d08da485e5608adfb99ae153aa95edcb724e
jonty282 <u23536862@tuks.co.za>
2026-07-08 18:02:22 +0200

    Unit test for the chat function using MockMagic() to see if database is receiving the correct information and the returned reply is correct

--- backend/tests/unit/test_ai_service.py
+++ backend/tests/unit/test_ai_service.py
@@ -1,5 +1,7 @@
-from app.services.ai_service import get_user_portfolio_context
+from app.services.ai_service import get_user_portfolio_context, chat
 from app.models.portfolio import Portfolios, Document
+from unittest.mock import MagicMock, patch
+from app.models.chat import ChatMessages
 
 def test_portfolio_linked_no_data(db_session, test_user):
     ai_reply = get_user_portfolio_context(db_session, test_user.id)
@@ -42,3 +44,26 @@ def test_portfolio_data_extraction_fail(db_session, test_user):
 
     output = get_user_portfolio_context(db_session, test_user.id)
     assert "portfolio.pdf" not in output
+
+
+@patch("app.services.ai_service.get_bedrock_client")
+def test_new_chat(mock_bedrock_client, db_session, test_user):
+    mocked_client = MagicMock()
+    mocked_client.converse.return_value = {
+        "output": {"message": {"content": [{"text": "A response."}] }}}
+    
+    mock_bedrock_client.return_value = mocked_client
+    reply, conversation_id = chat("A question?" ,db_session, test_user.id)
+
+    assert reply == "A response."
+    assert conversation_id is not None
+
+    messages = db_session.query(ChatMessages).filter_by(conversation_id = conversation_id).all()
+    assert len(messages) == 2
+    assert messages[0].role == "user"
+    assert messages[1].role == "assistant"
+    assert messages[0].content == "A question?"
+    assert messages[1].content == "A response."
+
+
+    
