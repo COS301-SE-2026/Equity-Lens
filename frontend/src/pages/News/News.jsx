c363ef3c17bce62f5a0f8a3d499c4a2986916618
abdulrahmansabah <152287517+abdulrahmansabah@users.noreply.github.com>
2026-08-18 20:29:35 +0200

    AS: Fix: Fixed the user selection for the news page

--- frontend/src/pages/News/News.jsx
+++ frontend/src/pages/News/News.jsx
@@ -146,13 +146,12 @@ const NewsInvestment = () => {
 
       </div>
 
-
       <div className="flex items-center mt-4 gap-2">
         <button
           onClick={() => setActiveTab("portfolio")}
           className={`flex items-center gap-2 px-4 py-2 rounded-md transition-colors border text-sm font-medium ${activeTab == "portfolio"
-              ? "bg-blue-500/20 text-blue-400 border-blue-500/40"
-              : "bg-transparent text-[var(--text-secondary)] border-[var(--border-subtle)]"
+            ? "bg-blue-500/20 text-blue-400 border-blue-500/40"
+            : "bg-transparent text-[var(--text-secondary)] border-[var(--border-subtle)]"
             }`}
         >
           <UserRound className="w-4 h-4" />
@@ -162,8 +161,8 @@ const NewsInvestment = () => {
         <button
           onClick={() => setActiveTab("watchlist")}
           className={`flex items-center gap-2 px-4 py-2 rounded-md transition-colors border text-sm font-medium ${activeTab == "watchlist"
-              ? "bg-blue-500/20 text-blue-400 border-blue-500/40"
-              : "bg-transparent text-[var(--text-secondary)] border-[var(--border-subtle)]"
+            ? "bg-blue-500/20 text-blue-400 border-blue-500/40"
+            : "bg-transparent text-[var(--text-secondary)] border-[var(--border-subtle)]"
             }`}>
           <Star className="w-4 h-4" />
           Watchlist
@@ -171,119 +170,86 @@ const NewsInvestment = () => {
 
         <button onClick={() => setActiveTab("market")}
           className={`flex items-center gap-2 px-4 py-2 rounded-md transition-colors border text-sm font-medium ${activeTab == "market"
-              ? "bg-blue-500/20 text-blue-400 border-blue-500/40"
-              : "bg-transparent text-[var(--text-secondary)] border-[var(--border-subtle)]"
+            ? "bg-blue-500/20 text-blue-400 border-blue-500/40"
+            : "bg-transparent text-[var(--text-secondary)] border-[var(--border-subtle)]"
             }`}>
           <UserRound className="w-4 h-4" />
           All Market
         </button>
       </div>
 
+      {activeTab === "portfolio" && (
+        <div className="mt-6 p-5 border border-[var(--border-subtle)] rounded-2xl">
+          <h2 className="text-xl font-semibold text-[var(--text-primary)] mb-4">
+            Latest news
+          </h2>
+          <p className="text-[var(--text-secondary)]">
+            Still to do porfolionews
+          </p>
+        </div>
+      )}
 
-      <div className="grid grid-cols-3 gap-6 mt-6">
+      {activeTab === "watchlist" && (
+        <div className="mt-6 p-5 border border-[var(--border-subtle)] rounded-2xl">
+          <h2 className="text-xl font-semibold text-[var(--text-primary)] mb-4">
+            Latest news
+          </h2>
+          <p className="text-[var(--text-secondary)]">
+            Still to do porfolionews watchlist
+          </p>
+        </div>
+      )}
 
-        <div className="col-span-2 p-5 border border-[var(--border-subtle)] rounded-2xl">
 
-          <h2 className="text-xl font-semibold text-[var(--text-primary)] mb-2">Latest News</h2>
-          <div>
-            <button onClick={() => ToGetTheNews("business")} className="px-3 py-1 rounded-full bg-[var(--surface-card)] text-[var(--text-secondary)] ml-4"> All </button>
-            <button onClick={() => ToGetTheNews("Top")} className="px-3 py-1 rounded-full bg-[var(--surface-card)] text-[var(--text-secondary)] ml-4"> Top </button>
-            <button onClick={() => ToGetTheNews("Business")} className="px-3 py-1 rounded-full bg-[var(--surface-card)] text-[var(--text-secondary)] ml-4"> Business </button>
-            <button onClick={() => ToGetTheNews("Technology")} className="px-3 py-1 rounded-full bg-[var(--surface-card)] text-[var(--text-secondary)] ml-4"> Technology </button>
-            <button onClick={() => ToGetTheNews("Politics")} className="px-3 py-1 rounded-full bg-[var(--surface-card)] text-[var(--text-secondary)] ml-4"> Politics </button>
-            <button onClick={() => ToGetTheNews("Crime")} className="px-3 py-1 rounded-full bg-[var(--surface-card)] text-[var(--text-secondary)] ml-4"> Crime </button>
-          </div>
+      {activeTab === "market" && (
+        <div className="mt-6">
 
-          {articles.map((article) => (
-            <div key={article.article_id} className="flex items-center  border-b border-[var(--border-subtle)] p-5 gap-4">
+          <div className="grid grid-cols-3 gap-6 mt-6">
 
+            <div className="col-span-3 p-5 border border-[var(--border-subtle)] rounded-2xl">
+
+              <h2 className="text-xl font-semibold text-[var(--text-primary)] mb-2">Latest News</h2>
               <div>
-                <img
-                  src={article.image_url}
-                  alt="news"
-                  className="w-20 h-20 rounded-lg object-cover" />
+                <button onClick={() => ToGetTheNews("business")} className="px-3 py-1 rounded-full bg-[var(--surface-card)] text-[var(--text-secondary)] ml-4"> All </button>
+                <button onClick={() => ToGetTheNews("Top")} className="px-3 py-1 rounded-full bg-[var(--surface-card)] text-[var(--text-secondary)] ml-4"> Top </button>
+                <button onClick={() => ToGetTheNews("Business")} className="px-3 py-1 rounded-full bg-[var(--surface-card)] text-[var(--text-secondary)] ml-4"> Business </button>
+                <button onClick={() => ToGetTheNews("Technology")} className="px-3 py-1 rounded-full bg-[var(--surface-card)] text-[var(--text-secondary)] ml-4"> Technology </button>
+                <button onClick={() => ToGetTheNews("Politics")} className="px-3 py-1 rounded-full bg-[var(--surface-card)] text-[var(--text-secondary)] ml-4"> Politics </button>
+                <button onClick={() => ToGetTheNews("Crime")} className="px-3 py-1 rounded-full bg-[var(--surface-card)] text-[var(--text-secondary)] ml-4"> Crime </button>
               </div>
 
+              {articles.map((article) => (
+                <div key={article.article_id} className="flex items-center  border-b border-[var(--border-subtle)] p-5 gap-4">
 
-              <div className="flex-1">
-                <h3 className="text-[var(--text-primary)]">{article.title}</h3>
-                <p className="text-sm text-[var(--text-secondary)] mt-1">{article.description}</p>
-                <p className="text-sm text-[var(--text-secondary)] mt-1">{article.pubDate}</p>
-                <p className="text-sm text-[var(--text-secondary)] mt-1">{article.source_name}</p>
-              </div>
+                  <div>
+                    <img
+                      src={article.image_url}
+                      alt="news"
+                      className="w-20 h-20 rounded-lg object-cover" />
+                  </div>
 
-              {article.category.map((article) => (
-                <p key={article} className="px-3 py-1 text-sm rounded-full" style={{ background: 'var(--signal-positive-bg)', color: 'var(--signal-positive)' }}>
-                  {article}
-                </p>
+
+                  <div className="flex-1">
+                    <h3 className="text-[var(--text-primary)]">{article.title}</h3>
+                    <p className="text-sm text-[var(--text-secondary)] mt-1">{article.description}</p>
+                    <p className="text-sm text-[var(--text-secondary)] mt-1">{article.pubDate}</p>
+                    <p className="text-sm text-[var(--text-secondary)] mt-1">{article.source_name}</p>
+                  </div>
+
+                  {article.category.map((article) => (
+                    <p key={article} className="px-3 py-1 text-sm rounded-full" style={{ background: 'var(--signal-positive-bg)', color: 'var(--signal-positive)' }}>
+                      {article}
+                    </p>
+                  ))}
+
+
+                </div>
               ))}
 
-
             </div>
-          ))}
-
-        </div>
-
-        <div className="flex flex-col gap-6">
-
-          <div className="p-5 border border-[var(--border-subtle)] rounded-2xl">
-
-            <div className="flex justify-between mb-4">
-              <h2 className="text-[var(--text-primary)] text-3xl">
-                My Watchlist
-              </h2>
-              <div className="flex items-center gap-3">
-                <input
-                  type="text"
-                  placeholder="Ticker"
-                  value={ticker}
-                  onChange={(e) => setTicker(e.target.value)}
-                  className="p-2 rounded bg-[var(--surface-card)] text-[var(--text-primary)] border border-[var(--border-subtle)]" />
-                <button onClick={() => AddStock(ticker)} className="px-4 bg-[var(--accent-primary)] hover:bg-[var(--accent-hover)] text-[var(--text-on-accent)] rounded-lg">
-                  Add
-                </button>
-              </div>
-            </div>
-            <table className="w-full">
-
-              <thead>
-                <tr>
-                  <th className="text-left py-3">Ticker</th>
-                  <th className="text-left  py-3">Price</th>
-                  <th className="text-left  py-3">Change</th>
-                  <th className="text-center py-3">Action</th>
-                </tr>
-              </thead>
-
-              <tbody>
-                {wishlist.length === 0 ? (<tr><td className="text-[var(--text-secondary)]">No watchlist stocks added </td></tr>) :
-                  (
-                    wishlist.map((items) => (
-                      <tr key={items.id} className="border-b border-[var(--border-subtle)] mb-7">
-
-                        <td>
-                          <p className="text-[var(--text-primary)]">{items.ticker}</p>
-                          <p className="text-[var(--text-secondary)]">{items.company_name}</p>
-                        </td>
-                        <td className="text-[var(--text-primary)]">
-                          {items.current_price}
-                        </td>
-                        <td className="text-[var(--signal-positive)]">
-                          {items.change_percent}
-                        </td>
-                        <td className="text-center">
-                          <button onClick={() => ToDeleteWishlist(items.id)} className="text-[var(--signal-negative)] hover:opacity-80">Remove</button>
-                        </td>
-                      </tr>
-                    ))
-                  )}
-              </tbody>
-
-            </table>
           </div>
-
         </div>
-      </div>
+      )}
     </div>
 
 
