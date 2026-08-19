7183a97ddac8d1c8a0de25263d0a0b4bb51e808a
Josh-Heath123 <joshuaheath004@gmail.com>
2026-08-19 23:03:03 +0200

    reusable blurred-money display comp

---
+++ frontend/src/components/common/Money/Money.jsx
@@ -0,0 +1,10 @@
+/**
+* @param {{ children: React.ReactNode, className?: string, as?: keyof JSX.IntrinsicElements }} props
+ */
+const Money = ({ children, className = '', as: Tag = 'span', ...rest }) => (
+  <Tag className={`money-value ${className}`.trim()} {...rest}>
+    {children}
+  </Tag>
+);
+
+export default Money;
