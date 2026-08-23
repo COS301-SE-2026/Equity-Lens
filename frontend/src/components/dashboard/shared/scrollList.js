bed1a5a01947f024c032e07de6fba9237eaba544
Josh-Heath123 <joshuaheath004@gmail.com>
2026-08-23 23:44:09 +0200

    New logo

--- frontend/src/components/dashboard/shared/scrollList.js
+++ frontend/src/components/dashboard/shared/scrollList.js
@@ -1,4 +1,3 @@
-export const SCROLL_LIST_CLASS = 'h-[380px] overflow-y-auto [&::-webkit-scrollbar]:hidden';
 export const SCROLL_LIST_FLEX_CLASS = 'min-h-0 flex-1 overflow-y-auto [&::-webkit-scrollbar]:hidden';
 
 /** @type {React.CSSProperties} */
