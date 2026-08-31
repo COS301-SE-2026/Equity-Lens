3d860ee64029eaca4c67ee9b0e255839e7ea5fbe
abdulrahmansabah <152287517+abdulrahmansabah@users.noreply.github.com>
2026-08-31 23:17:35 +0200

    AS: Fix: Fixed pipelines issues

--- backend/tests/K6/scalability_nfr_4.1.js
+++ backend/tests/K6/scalability_nfr_4.1.js
@@ -10,7 +10,7 @@ export const options = {
     thresholds: {
         http_req_duration: ["p(95)<2000"],
         http_req_failed: ["rate<0.01"],
-        checks: ["rate==0.1"]
+        checks: ["rate==1.0"]
     }
 }
 
