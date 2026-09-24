e6a9a1412fd26564cc0416842878439749d6d564
antonyvs05 <vanstratenantony@gmail.com>
2026-09-24 12:49:30 +0200

    feat: Added seeded list of JSE stocks to refer to

---
+++ backend/app/services/index_universe.py
@@ -0,0 +1,5 @@
+SEED_UNIVERSE: list[str] = [
+    "NPN", "PRX", "CFR", "BTI", "AGL", "ANG", "GFI", "FSR", "CPI", "SBK",
+    "ABG", "NED", "MTN", "VOD", "SOL", "SSW", "IMP", "MRP", "WHL", "SHP",
+    "BID", "BVT", "DSY", "GRT", "REM", "EXX", "APN", "BHG", "OMU", "CLS",
+]
